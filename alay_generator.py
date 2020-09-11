0b038bcba2279384f3d92f87a01c67cfc5dbb076
Jaka Pratama <jakakupratama44@gmail.com>
2020-09-12 02:09:25 +0700

    Create alay_generator.py

---
+++ alay_generator.py
@@ -0,0 +1,45 @@
+def alayGenerator(sentence_input):
+    alay_dict = {
+        'a': '4',
+        'i': '1',
+        'e': '3',
+        'g': '6',
+        'j': 'jh',
+        'o': '0',
+        's': '5',
+        'u': 'uw',
+        'c': 'ch',
+        'f': 'v',
+        'k': 'q',
+        'p': 'ph',
+        't': 'th',
+        'y': 'iy'
+    }
+
+    result = []
+
+    for word in sentence_input:
+        for char in word:
+            result.append(char)
+            for alay in alay_dict:
+                if (char == alay):
+                    result.pop()
+                    result.append(alay_dict[char])
+
+    return ''.join(result)
+
+
+if __name__ == '__main__':
+    sentence_input = input("\nq4l1m4th y6 m4u d1ub4h: ").split(sep=' ')
+    alay_generated = []
+
+    for i in range(len(sentence_input)):
+        alay_generated.append(alayGenerator(sentence_input[i]))
+        print(alay_generated)
+
+    alay_words_join = ' '.join(alay_generated)
+
+    print("\nHasilnya: ")
+    print("="*len(alay_words_join))
+    print(alay_words_join)
+    print("="*len(alay_words_join))
